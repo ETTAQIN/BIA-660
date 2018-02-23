8fb7a2ca37b5123707f2b909f292698992345631
qinjiale <jqin2@stevens.edu>
2018-02-23 15:01:53 -0500

    first commit

--- Assignment_01/test_questions.py
+++ Assignment_01/test_questions.py
@@ -2,5 +2,9 @@ from information_extraction import answer_question, process_data_from_input_file
 
 process_data_from_input_file()
 answer_question('Who has a dog?')
-print()
+print('')
 answer_question('Who likes Mary?')
+print('')
+answer_question('Does Chris like Mary?')
+print('')
+answer_question('Who is going to Mexico?')
